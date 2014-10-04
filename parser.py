a63aca4a6dcc733e17cdeccd72c9803b3fe6ed71
netbofia <netbofia@hotmail.com>
2014-10-04 20:58:18 +0100

    Type global name secondary

--- parser.py
+++ parser.py
@@ -3,24 +3,24 @@
 # -*- coding: utf-8 -*-
 #
 #  parser.py
-#  
+#
 # Copyright 2014 CoBiG^2 <f.pinamartins@gmail.com>
-#  
-#  This program is free software; you can redistribu    te it and/or modify
+#
+#  This program is free software; you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
 #  the Free Software Foundation; either version 2 of the License, or
 #  (at your option) any later version.
-#  
+#
 #  This program is distributed in the hope that it will be useful,
 #  but WITHOUT ANY WARRANTY; without even the implied warranty of
 #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #  GNU General Public License for more details.
-#  
+#
 #  You should have received a copy of the GNU General Public License
 #  along with this program; if not, write to the Free Software
 #  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
 #  MA 02110-1301, USA.
-#  
+#
 #  parser.py v1.0
 #  Author: Bruno Costa
 #  Author: Francisco P Martins
@@ -60,7 +60,7 @@ class noname(object):
     def __init__(self):
         #loads data
         primary=[]
-        secundary=[]
+        secondary=[]
         for i in tag_files:
             tag=open(i,"r")
             for tag_line in tag:
