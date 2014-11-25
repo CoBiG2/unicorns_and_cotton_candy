93c60e5c32147dc7d9bb1a88d8b5df4660280207
Francisco Pina Martins <f.pinamartins@gmail.com>
2014-11-25 12:12:31 +0000

    Added a Licence header.

--- Plotters/freq_plotter.py
+++ Plotters/freq_plotter.py
@@ -1,4 +1,22 @@
 #!/usr/bin/python3
+#  freq_ploter.py
+#
+# Copyright 2014 CoBiG^2 <f.pinamartins@gmail.com>
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
 
 #Usage: python3 freq_plotter freq_file.txt
 
@@ -68,6 +86,6 @@ def comul_plotter(d):
     plt.tight_layout()
     plt.show()
 
-
-d = parser(argv[1])
-comul_plotter(d)
+if __name__ == "__main__":
+    d = parser(argv[1])
+    comul_plotter(d)
